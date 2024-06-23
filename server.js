f7c3b5c88575b261e510c310d5d75540fffe9d82
sageeth007 <sageeth007@gmail.com>
2024-06-23 15:58:18 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
