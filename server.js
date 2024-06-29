22feee22ea7afe0fb059c08be56adc8c6c620660
sageeth007 <sageeth007@gmail.com>
2024-06-29 13:36:19 +0100

    Mongo DB comment

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+// const mongoose = require('mongoose');
 require('dotenv').config();
 
 const app = express();
@@ -8,6 +9,12 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
+
+/* mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log("MongoDB database connection established successfully");
+}); */
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
