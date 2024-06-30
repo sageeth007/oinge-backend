878c66de65ee189abdb26b400ec834c30db47399
sageeth007 <sageeth007@gmail.com>
2024-06-30 12:12:02 +0100

    changed port to 8080

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(bodyParser.json());
